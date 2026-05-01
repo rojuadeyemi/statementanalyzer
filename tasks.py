02fc909ad5b49ac9618d1073949a9ebe43c3d424
Aderoju Adeyemi <64233556+rojuadeyemi@users.noreply.github.com>
2026-05-01 13:53:53 +0100

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,16 +1,18 @@
-from analyzer.analyzer import Analyzer
-from redis_conn import conn
-import json
-
 def run_analysis(job_id, filepath, filename):
+    print("JOB STARTED:", job_id)
+
     try:
         conn.set(job_id, json.dumps({"status": "Reading statement..."}))
+        print("STEP 1")
 
         statement = Analyzer(filepath)
 
         conn.set(job_id, json.dumps({"status": "Analyzing transactions..."}))
+        print("STEP 2")
 
         summary = statement.risk_indicators().to_dict()
+
+        print("STEP 3 DONE")
 
         conn.set(job_id, json.dumps({
             "status": "Completed",
@@ -19,7 +21,10 @@ def run_analysis(job_id, filepath, filename):
             "filename": filename
         }))
 
+        print("JOB COMPLETED")
+
     except Exception as e:
+        print("JOB ERROR:", str(e))
         conn.set(job_id, json.dumps({
             "status": "Error",
             "result": str(e)
