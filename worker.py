5179c8e3e16a31ca6eff2e22615af149027620bc
Aderoju Adeyemi <64233556+rojuadeyemi@users.noreply.github.com>
2026-05-01 14:13:14 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -2,7 +2,7 @@ from rq import Worker, Queue
 from redis_conn import conn
 
 listen = ['default']
-
+print("WORKER REDIS:", conn)
 if __name__ == '__main__':
     worker = Worker([Queue(name, connection=conn) for name in listen])
     worker.work()
